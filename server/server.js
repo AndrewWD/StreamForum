60abe57ffc335e50b61634b586acf3f250ddc173
Yang Gao <yg1751@nyu.edu>
2018-10-03 01:16:23 -0400

    setup the request proxy

--- server/server.js
+++ server/server.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const favicon = require('serve-favicon')
-const ReactSSR  = require('react-dom/server')
+const bodyParser = require('body-parser')
+const session = require('express-session')
+const ReactSSR = require('react-dom/server')
 const fs = require('fs')
 const path = require('path')
 
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(session({
+  maxAge: 10 * 60 * 1000,
+  name: 'tid',
+  resave: false,
+  saveUninitialized: false,
+  secret: 'stream forum',
+}))
 app.use(favicon(path.join(__dirname, '../favicon.ico')))
+app.use('/api/user', require('./utils/handle-login'))
+app.use('/api', require('./utils/proxy'))
+
 const isDev = process.env.NODE_ENV === 'development'
 if (!isDev) {
   const serverEntry = require('../dist/server-entry').default
@@ -13,7 +28,7 @@ if (!isDev) {
 
   app.use('/public', express.static(path.join(__dirname, '../dist')))
 
-  app.get('*', function(req, res) {
+  app.get('*', function (req, res) {
     const appString = ReactSSR.renderToString(serverEntry)
     const resHtml = templateHtml.replace('<!-- app -->', appString)
     res.send(resHtml)
@@ -23,6 +38,6 @@ if (!isDev) {
   devStatic(app)
 }
 
-app.listen(3333, function() {
+app.listen(3333, function () {
   console.log('server is listening on 3333')
 })
