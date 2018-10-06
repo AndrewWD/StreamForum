9f060cdce68f16cca0c92b4943625699a0b5bf7e
Yang Gao <yg1751@nyu.edu>
2018-10-06 01:00:42 -0400

    change the method of compiling modules in server dev mode

--- server/utils/dev-static.js
+++ server/utils/dev-static.js
@@ -21,7 +21,21 @@ const getTemplate = () => {
   })
 }
 
-const Module = module.constructor
+const NativeModule = require('module')
+const vm = require('vm')
+
+const getModuleFromString = (bundle, filename) => {
+  const m = { exports: {} }
+  const wrapper = NativeModule.wrap(bundle)
+  const script = new vm.Script(wrapper, {
+    filename: filename,
+    displayErrors: true,
+  })
+  const result = script.runInThisContext()
+  result(m.exports, require, m)
+  return m
+}
+
 const mfs = new MemoryFs()
 serverConfig.mode = 'development'
 const serverCompiler = webpack(serverConfig)
@@ -34,8 +48,7 @@ serverCompiler.watch({}, (err, stats) => {
 
   const bundlePath = path.join(serverConfig.output.path, serverConfig.output.filename)
   const bundle = mfs.readFileSync(bundlePath, 'utf-8')
-  const m = new Module()
-  m._compile(bundle, 'serverEntry.js')
+  const m = getModuleFromString(bundle, 'serverEntry.js')
   serverEntry = m.exports.default
   createStoreMap = m.exports.createStoreMap
 })
